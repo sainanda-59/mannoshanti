038f499b8d1f17374d6432a8987f917f20b3115e
sainanda-59 <91083389+sainanda-59@users.noreply.github.com>
2021-10-23 23:14:39 +0530

    project complete

---
+++ meri-diary/src/App.js
@@ -0,0 +1,55 @@
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import Entry from './components/Entry/Entry'
+import db from './firebase';
+import firebase from 'firebase';
+import './App.css';
+
+
+
+function App() {
+  const [entries,setEntries] = useState([]);
+  const [input,setInput] = useState('');
+
+  useEffect(() => {
+    db.collection('entries').orderBy('timestamp','desc').onSnapshot(snapshot => {
+         setEntries(snapshot.docs.map(doc => ({id:doc.id,entry:doc.data().entry})));
+    })
+
+  }, []);
+
+
+  const addEntry = (event) => {
+         event.preventDefault();
+         db.collection('entries').add({
+           entry: input,
+           timestamp: firebase.firestore.FieldValue.serverTimestamp()
+         })
+         setEntries([...entries, input]);
+         setInput('');
+  }
+  return (
+    <div className="App">
+      <h1 className="head-title">Meri Diary <i class="fas fa-book"></i></h1>
+      <form>
+    <FormControl>
+      <InputLabel className="input-label">How was your Day?</InputLabel>
+    <Input className="input-text" value={input} onChange={event => setInput(event.target.value)}/>
+    </FormControl>
+    <Button className="submit-btn" disabled={!input} variant="contained" color="primary" type="submit" onClick={addEntry}>
+    <i class="fas fa-plus-square fa-lg"></i>
+    </Button>
+    </form>
+    
+    <div className="entries-container">
+      {entries.map(entry =>(
+
+        <Entry entry={entry} />
+
+      ))}
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ meri-diary/src/components/Entry/Entry.js
@@ -0,0 +1,51 @@
+import { Button, FormControl, Input, InputLabel, Modal } from '@material-ui/core';
+import React, { useState } from 'react'
+import db from '../../firebase';
+import ('./Entry.css');
+
+
+function Entry({entry}) {
+    const[open,setOpen] = useState(false);
+    const[input,setInput]= useState(entry.entry);
+
+    const updateEntry = (event)=>{
+        event.preventDefault();
+        db.collection('entries').doc(entry.id).set({
+              entry : input
+        },{merge:true})
+        setOpen(false);
+    }
+
+    return (
+       <>
+        <Modal
+        open={open}
+        onClose={e => setOpen(false)}
+        >
+            <div className="update-box" >
+            <h2>Edit Diary</h2>
+            <form>
+            <FormControl>
+            <InputLabel className="input-label">Missed Something?</InputLabel>
+            <Input className="update-text" value={input} onChange={event => setInput(event.target.value)}/>
+            </FormControl>
+            <Button type="submit" className="update-btn" onClick={updateEntry} variant="contained" color="primary"><i class="far fa-save"></i>Save</Button>
+            </form>
+            </div>
+        </Modal>
+        
+        <div className="entry">
+            <div className="edit-delete">
+            <Button className="edit-btn" onClick={e => setOpen(true)} variant="contained" color="primary"><i class="fas fa-edit"></i> Edit</Button>
+            <Button className="delete-btn" variant="contained" color="secondary" onClick={event => db.collection('entries').doc(entry.id).delete() }> <i class="fas fa-trash-alt"></i> Delete</Button>
+            </div>
+            <div className="entry-text">
+                {entry.entry}
+            </div>
+            
+        </div>
+        </>
+    )
+}
+
+export default Entry
